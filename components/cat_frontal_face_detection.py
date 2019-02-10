b7b7bfc99ec8094ca51cd773bc6e268ec8f0a551
Bruce <brucewmacdonald@gmail.com>
2019-02-09 21:19:20 -0800

    dlib predictor and detector

--- components/cat_frontal_face_detection.py
+++ components/cat_frontal_face_detection.py
@@ -10,26 +10,40 @@ from constants.constants import debug_cat_frontal_face_detection
 
 
 # pre-trained classifier from OpenCV
-CLASSIFIER = 'data/haarcascade_frontalcatface.xml'
+DETECTOR = 'data/cat_face_detector.svm'
 # Pre-trained shape predictor from iBUG 300-W dataset for human facial landmarks
-SHAPE_PREDICTOR = 'data/shape_predictor_68_face_landmarks.dat'
+SHAPE_PREDICTOR = 'data/cat_landmark_predictor.dat'
 
-detector = cv2.CascadeClassifier(CLASSIFIER)
+detector = dlib.fhog_object_detector(DETECTOR)
 landmarks_predictor = dlib.shape_predictor(SHAPE_PREDICTOR)
 
 
+# convenience function from imutils
+def dlib_to_cv_bounding_box(box):
+    # convert dlib bounding box for OpenCV display
+    x = box.left()
+    y = box.top()
+    w = box.right() - x
+    h = box.bottom() - y
+
+    return x, y, w, h
+
+
 def show_detected_faces(img, bounding_boxes, facial_landmark_points):
-    for (x, y, w, h) in bounding_boxes:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    for face in bounding_boxes:
+        # draw box for face
+        x, y, w, h = dlib_to_cv_bounding_box(face)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # draw circles for landmarks
-    for x, y in facial_landmark_points:
-        cv2.circle(img, (x, y), 1, (0, 0, 255), -1)
+        # draw circles for landmarks
+        for landmark_set in facial_landmark_points:
+            for x, y in landmark_set:
+                cv2.circle(img, (x, y), 1, (0, 0, 255), -1)
 
-    cv2.imshow("Detected cat faces: ", img)
-    cv2.waitKey(0)
-
-    cv2.destroyAllWindows()
+        # show the output image with the face detections + facial landmarks
+        cv2.imshow("Output", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 # conversion from imutils
@@ -51,18 +65,12 @@ def detect_cat_face(img):
 
     # pre-process the image to grayscale, a normal step for haar classifiers
     grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # set a high scale factor because we care more about performance than accuracy
-    # optional: use minNeighbors to decrease false positives by increasing threshold num bounding boxes
-    # to be considered a face
-    # use minSize to ignore faces that are too small, need to test different values here based on usage
-    face_bounding_boxes = detector.detectMultiScale(grayscale_img, scaleFactor=1.7, minNeighbors=1, minSize=(75, 75))
 
-    # TODO: currently testing a human facial landmark detector on the cat faces,
-    #       will probably need to train a new landmark detector
-    # for face in face_bounding_boxes:
-    # TODO: need to convert face to the form: rectangles[[(241, 170) (562, 491)]]
-    # landmarks = landmarks_predictor(img, cv_face)
-    # facial_landmark_points.append(landmarks_to_numpy(landmarks))
+    face_bounding_boxes = detector(grayscale_img, 1)
+
+    for face in face_bounding_boxes:
+        landmarks = landmarks_predictor(img, face)
+        facial_landmark_points.append(landmarks_to_numpy(landmarks))
 
     if debug_cat_frontal_face_detection:
         show_detected_faces(img, face_bounding_boxes, facial_landmark_points)
